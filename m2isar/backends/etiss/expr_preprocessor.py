b2fbd01a9f5cf1fbe0adaeac13f705ba4b9f961c
wysiwyng <4764286+wysiwyng@users.noreply.github.com>
2022-05-17 08:59:54 +0200

    preprocess scalar then function staticness

--- m2isar/backends/etiss/expr_preprocessor.py
+++ m2isar/backends/etiss/expr_preprocessor.py
@@ -6,10 +6,15 @@ from . import scalar_staticness, function_staticness
 logger = logging.getLogger("preprocessor")
 
 def process_functions(core: arch.CoreDef):
-	patch_model(function_staticness)
-
 	for fn_name, fn_def in core.functions.items():
-		logger.info("examining staticness for fn %s", fn_name)
+		patch_model(scalar_staticness)
+
+		logger.info("examining scalar staticness for fn %s", fn_name)
+		fn_def.operation.generate(None)
+
+		patch_model(function_staticness)
+
+		logger.info("examining function staticness for fn %s", fn_name)
 
 		if arch.FunctionAttribute.ETISS_NEEDS_ARCH in fn_def.attributes and arch.FunctionAttribute.ETISS_STATICFN in fn_def.attributes:
 			raise ValueError("etiss_needs_arch and etiss_staticfn not allowed together")
@@ -24,12 +29,6 @@ def process_functions(core: arch.CoreDef):
 		else:
 			ret = fn_def.operation.generate(None)
 			fn_def.static = ret
-
-	patch_model(scalar_staticness)
-
-	for fn_name, fn_def in core.functions.items():
-		logger.info("examining staticness for fn %s", fn_name)
-		fn_def.operation.generate(None)
 
 def process_instructions(core: arch.CoreDef):
 	patch_model(scalar_staticness)
