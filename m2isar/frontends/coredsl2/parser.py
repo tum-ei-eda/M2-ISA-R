29db2b4f77e0eb6db70128db5f6af2cde68088ff
wysiwyng <4764286+wysiwyng@users.noreply.github.com>
2023-10-02 18:41:57 +0200

    remove pc inc, must be provided by always block

--- m2isar/frontends/coredsl2/parser.py
+++ m2isar/frontends/coredsl2/parser.py
@@ -243,7 +243,7 @@ def main():
 				)
 			)
 
-			op.statements.insert(0, pc_inc)
+			#op.statements.insert(0, pc_inc)
 			op.statements = always_block_statements + op.statements
 			instr_def.operation = op
 
