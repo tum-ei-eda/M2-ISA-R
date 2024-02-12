11573f9dcccc7cb1d3d29038e400249d413fa516
Philipp van Kempen <philipp.van-kempen@tum.de>
2024-02-12 12:40:48 +0100

    metamodel/behav.py: add inferred_type field to BaseNode

--- m2isar/metamodel/behav.py
+++ m2isar/metamodel/behav.py
@@ -33,6 +33,7 @@ class BaseNode:
 
 	def __init__(self, line_info: "LineInfo"=None) -> None:
 		self.line_info = line_info
+		self.inferred_type = None
 
 	def generate(self, context):
 		raise NotImplementedError()
