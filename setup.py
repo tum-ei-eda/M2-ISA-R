952d3043d5a8a84eac81d9c8697eb1d653801d69
wysiwyng <4764286+wysiwyng@users.noreply.github.com>
2024-03-13 17:30:03 +0100

    add coverage tool to setup.py

--- setup.py
+++ setup.py
@@ -9,24 +9,25 @@
 import setuptools
 
 setuptools.setup(
-    name="m2isar",
-    url="https://github.com/tum-ei-eda/m2-isa-r",
-    use_scm_version=True,
-    packages=setuptools.find_packages(),
-    package_data={
-        "": ["*.mako"]
-    },
-    setup_requires=["setuptools_scm"],
-    install_requires=[
-        "mako",
-        "antlr4-python3-runtime == 4.13.1"
-    ],
-    entry_points={
-        "console_scripts": [
-            "etiss_writer=m2isar.backends.etiss.writer:main",
-            "coredsl2_parser=m2isar.frontends.coredsl2.parser:main",
-            "m2isar_viewer=m2isar.backends.viewer.viewer:main"
-        ]
-    },
-    zip_safe=False
+	name="m2isar",
+	url="https://github.com/tum-ei-eda/m2-isa-r",
+	use_scm_version=True,
+	packages=setuptools.find_packages(),
+	package_data={
+		"": ["*.mako"]
+	},
+	setup_requires=["setuptools_scm"],
+	install_requires=[
+		"mako",
+		"antlr4-python3-runtime == 4.13.1"
+	],
+	entry_points={
+		"console_scripts": [
+			"etiss_writer=m2isar.backends.etiss.writer:main",
+			"coredsl2_parser=m2isar.frontends.coredsl2.parser:main",
+			"m2isar_viewer=m2isar.backends.viewer.viewer:main",
+			"m2isar2lcov=m2isar.backends.coverage.coverage_lcov:main"
+		]
+	},
+	zip_safe=False
 )
