a28a8a548561dd4377f7d39a4e36aeace1a4cd43
Philipp van Kempen <philipp.van-kempen@tum.de>
2026-04-13 08:57:16 +0200

    handle ENABLE attribute in parser (before duplicate detection)

--- m2isar/metamodel/arch.py
+++ m2isar/metamodel/arch.py
@@ -560,7 +560,7 @@ class CoreDef(Named):
 	"""A class representing an entire CPU core. Contains the collected attributes of multiple InstructionSets."""
 
 	def __init__(self, name, contributing_types: "list[str]", template: str, constants: "dict[str, Constant]", memories: "dict[str, Memory]",
-			memory_aliases: "dict[str, Memory]", functions: "dict[str, Function]", instructions: "dict[tuple[int, int], Instruction]",
+			memory_aliases: "dict[str, Memory]", functions: "dict[str, Function]", instructions: "dict[tuple[int, int], Instruction] | list[Instruction]",
 			instr_classes: "set[int]", intrinsics: "dict[str, Intrinsic]"):
 
 		self.contributing_types = contributing_types
@@ -581,13 +581,9 @@ class CoreDef(Named):
 		self.irq_pending_memory = None
 		self.intrinsics = intrinsics
 
-		self.instructions_by_ext = defaultdict(dict)
+		self._instructions_by_ext = None
 		self.functions_by_ext = defaultdict(dict)
-		self.instructions_by_class = defaultdict(dict)
-
-		for (code, mask), instr_def in self.instructions.items():
-			self.instructions_by_ext[instr_def.ext_name][(code, mask)] = instr_def
-			self.instructions_by_class[instr_def.size][(code, mask)] = instr_def
+		self._instructions_by_class = None
 
 		for fn_name, fn_def in self.functions.items():
 			self.functions_by_ext[fn_def.ext_name][fn_name] = fn_def
@@ -609,3 +605,23 @@ class CoreDef(Named):
 				self.irq_pending_memory = mem
 
 		super().__init__(name)
+
+	@property
+	def instructions_by_ext(self):
+		if self._instructions_by_ext is not None:
+			return self._instructions_by_ext
+		assert isinstance(self.instructions, dict)
+		self._instructions_by_ext = defaultdict(dict)
+		for (code, mask), instr_def in self.instructions.items():
+			self._instructions_by_ext[instr_def.ext_name][(code, mask)] = instr_def
+		return self._instructions_by_ext
+
+	@property
+	def instructions_by_class(self):
+		if self._instructions_by_class is not None:
+			return self._instructions_by_class
+		assert isinstance(self.instructions, dict)
+		self._instructions_by_class = defaultdict(dict)
+		for (code, mask), instr_def in self.instructions.items():
+			self._instructions_by_class[instr_def.size][(code, mask)] = instr_def
+		return self._instructions_by_class
