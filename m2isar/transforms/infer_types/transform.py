963db922d415df88ac08a1e1f37d286e6c9ccbca
Philipp van Kempen <philipp.van-kempen@tum.de>
2026-04-15 17:01:22 +0200

    add m2isar/transforms/infer_types

---
+++ m2isar/transforms/infer_types/transform.py
@@ -0,0 +1,69 @@
+# SPDX-License-Identifier: Apache-2.0
+#
+# This file is part of the M2-ISA-R project: https://github.com/tum-ei-eda/M2-ISA-R
+#
+# Copyright (C) 2022
+# Chair of Electrical Design Automation
+# Technical University of Munich
+
+"""Type inference for M2-ISA-R metamodel."""
+
+import sys
+import argparse
+import logging
+import pathlib
+
+from m2isar.metamodel import patch_model, load_model, dump_model
+
+from . import visitor
+
+
+def get_parser():
+    # read command line args
+    parser = argparse.ArgumentParser()
+    parser.add_argument("top_level", help="A .m2isarmodel file.")
+    parser.add_argument("--log", default="info", choices=["critical", "error", "warning", "info", "debug"])
+    parser.add_argument("--output", "-o", type=str, default=None)
+    return parser
+
+
+def run(args):
+    # initialize logging
+    logging.basicConfig(level=getattr(logging, args.log.upper()))
+    logger = logging.getLogger("infer_types")
+    logger.setLevel(getattr(logging, args.log.upper()))
+
+    # resolve model paths
+    top_level = pathlib.Path(args.top_level)
+
+    out_path = (top_level.parent / top_level.stem) if args.output is None else args.output
+    print("out_path", out_path)
+
+    model_obj = load_model(top_level)
+
+    for _, core_def in model_obj.cores.items():
+        logger.debug("inferring types for core %s", core_def.name)
+        patch_model(visitor)
+        for _, instr_def in core_def.instructions.items():
+            logger.debug("inferring types for instr %s", instr_def.name)
+            instr_def.operation.generate(None)
+            # input("!")
+    for _, set_def in model_obj.sets.items():
+        logger.debug("inferring types for set %s", set_def.name)
+        patch_model(visitor)
+        for _, instr_def in set_def.instructions.items():
+            logger.debug("inferring types for instr %s", instr_def.name)
+            instr_def.operation.generate(None)
+            # input("!")
+
+    dump_model(model_obj, out_path)
+
+
+def main(argv):
+    parser = get_parser()
+    args = parser.parse_args(argv)
+    run(args)
+
+
+if __name__ == "__main__":
+    main(sys.argv[1:])
