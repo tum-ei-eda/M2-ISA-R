da40bfa9740c440b29c3dd91817aa5257cb3be36
Philipp van Kempen <philipp.van-kempen@tum.de>
2026-06-19 13:37:37 +0200

    m2isar/backends/etiss/instruction_transform.py: if type conv expr is literal, the results is literal too

--- m2isar/backends/etiss/instruction_transform.py
+++ m2isar/backends/etiss/instruction_transform.py
@@ -635,6 +635,7 @@ class InstructionTransformVisitor(ExprVisitor):
 			code_str = f'({data_type_map[expr.data_type]}{expr.actual_size})({code_str})'
 
 		c = CodeString(code_str, expr_str.static, expr.size, expr.data_type == arch.DataType.S, expr_str.regs_affected, line_infos=[expr.line_info] + expr_str.line_infos)
+		c.is_literal = expr_str.is_literal
 		c.mem_ids = expr_str.mem_ids
 
 		return c
